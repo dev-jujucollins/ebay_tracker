fc87dd8552a52a4f185e84bdb5bb3cfe3113836b
Julius Collins <collinsjulius1994@gmail.com>
2025-04-24 20:44:00 -0700

    Refactor main.py to improve argument parsing and validation for eBay search urls

--- main.py
+++ main.py
@@ -1,20 +1,23 @@
 #!/usr/bin/env python3
+import sys
 import numpy as np
-from utils import logging, get_prices_by_link, remove_outliers, get_average, extract_item_name, save_to_file
+from utils import logging, get_prices_by_link, remove_outliers, get_average, save_to_file, validate_url, get_item_name, parse_arguments_and_generate_link
 
 if __name__ == "__main__":
-    link = input("Enter the eBay search link: ")
-    print()
-    if not link.strip():
-        logging.error("No link provided. Please provide a valid eBay search link.")
+    link, item_name = parse_arguments_and_generate_link(sys.argv)
+    
+    if not link:
+        link = input("Enter an eBay search URL: ").strip()
+        if not link:
+            logging.error("No link provided. Please provide a valid eBay search link.")
+            exit(1)
+
+    if not validate_url(link):
         exit(1)
 
-    item_name = extract_item_name(link)
+    item_name = get_item_name(link, item_name)
     if not item_name:
-        item_name = input("Enter the name of the item: ").strip()
-        if not item_name:
-            logging.error("No item name provided. Exiting.")
-            exit(1)
+        exit(1)
 
     prices = get_prices_by_link(link)
     prices_without_outliers = remove_outliers(prices)
